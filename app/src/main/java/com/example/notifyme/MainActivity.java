bd5c928a8798f26f03c660db6b144454e20217b6
Oscar Anyiam <oscaranyiam94@gmail.com>
2020-11-16 17:07:42 +0100

    A simple notification

---
+++ app/src/main/java/com/example/notifyme/MainActivity.java
@@ -0,0 +1,70 @@
+package com.example.notifyme;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
+
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    NotificationManager notificationManager;
+    private static  final String PRIMARY_CHANNEL_ID = "primary_notification_channel";
+    private  static  final int NOTIFICATION_ID = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button notify = findViewById(R.id.button_notify);
+        createNotification();
+
+
+    }
+
+    private void sendNotification() {
+       NotificationCompat.Builder notifyBuilder = getNotificationBuilder();
+        notificationManager.notify(NOTIFICATION_ID, notifyBuilder.build());
+    }
+
+    private NotificationCompat.Builder getNotificationBuilder(){
+
+        Intent intent = new Intent(this, MainActivity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, NOTIFICATION_ID, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, PRIMARY_CHANNEL_ID)
+                .setContentTitle("Hey")
+                .setContentText("Another dsy to code")
+                .setSmallIcon(R.drawable.ic_android)
+                .setContentIntent(pendingIntent)
+                .setAutoCancel(true);
+
+        return  builder;
+    }
+
+    public void onCLick(View view) {
+        sendNotification();
+    }
+
+  public void  createNotification(){
+        notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+            NotificationChannel notificationChannel = new NotificationChannel(
+                    PRIMARY_CHANNEL_ID, "Mascot Notifications", NotificationManager.IMPORTANCE_HIGH);
+
+            notificationChannel.setLightColor(Color.RED);
+            notificationChannel.setDescription("Noitificstion from mascot");
+            notificationChannel.enableLights(true);
+            notificationChannel.enableVibration(true);
+            notificationManager.createNotificationChannel(notificationChannel);
+
+        }
+    }
+}
