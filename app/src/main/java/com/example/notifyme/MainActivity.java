4da112b68df15c9c3d545f2e434f6e3666ade153
Oscar Anyiam <oscaranyiam94@gmail.com>
2020-11-16 18:23:05 +0100

    Updated the App to cancel and update notifications

--- app/src/main/java/com/example/notifyme/MainActivity.java
+++ app/src/main/java/com/example/notifyme/MainActivity.java
@@ -8,6 +8,8 @@ import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Color;
 import android.os.Build;
 import android.os.Bundle;
@@ -19,19 +21,78 @@ public class MainActivity extends AppCompatActivity {
     NotificationManager notificationManager;
     private static  final String PRIMARY_CHANNEL_ID = "primary_notification_channel";
     private  static  final int NOTIFICATION_ID = 0;
+    private Button updateNotification;
+    private Button cancelNotification;
+    private Button notify;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button notify = findViewById(R.id.button_notify);
+        notify = findViewById(R.id.button_notify);
+        updateNotification = findViewById(R.id.button_update);
+        cancelNotification = findViewById(R.id.button_cancel);
         createNotification();
+
+        setNotificationButtonState(true,false, false);
+
+        sendNotification();
+
+
+        updateNotification();
+
+        cancelNotification();
 
 
     }
 
+
+    private void cancelNotification() {
+        cancelNotification.setOnClickListener(v->{
+            cancel();
+            setNotificationButtonState(true, false, false);
+        });
+
+
+    }
+
+    private void cancel() {
+        notificationManager.cancel(NOTIFICATION_ID);
+    }
+
+    private void updateNotification() {
+        updateNotification.setOnClickListener(v->{
+            update();
+            setNotificationButtonState(false, false, true);
+        });
+
+    }
+
+    private  void setNotificationButtonState(Boolean isNotify, Boolean isUpdate, Boolean isCanceled){
+       notify.setEnabled(isNotify);
+       updateNotification.setEnabled(isUpdate);
+       cancelNotification.setEnabled(isCanceled);
+    }
+
+    private void update() {
+        Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.mascot_1);
+        NotificationCompat.Builder builder = getNotificationBuilder();
+        builder.setStyle(new NotificationCompat.BigPictureStyle()
+                .bigPicture(bitmap).setBigContentTitle("Notification Update"));
+        notificationManager.notify(NOTIFICATION_ID, builder.build());
+    }
+
     private void sendNotification() {
-       NotificationCompat.Builder notifyBuilder = getNotificationBuilder();
+        notify.setOnClickListener(v->{
+            send();
+            setNotificationButtonState(false, true, true);
+
+        });
+
+    }
+
+    private void send() {
+        NotificationCompat.Builder notifyBuilder = getNotificationBuilder();
         notificationManager.notify(NOTIFICATION_ID, notifyBuilder.build());
     }
 
@@ -41,7 +102,7 @@ public class MainActivity extends AppCompatActivity {
         PendingIntent pendingIntent = PendingIntent.getActivity(this, NOTIFICATION_ID, intent, PendingIntent.FLAG_UPDATE_CURRENT);
         NotificationCompat.Builder builder = new NotificationCompat.Builder(this, PRIMARY_CHANNEL_ID)
                 .setContentTitle("Hey")
-                .setContentText("Another dsy to code")
+                .setContentText("Another day to code")
                 .setSmallIcon(R.drawable.ic_android)
                 .setContentIntent(pendingIntent)
                 .setAutoCancel(true)
@@ -53,6 +114,7 @@ public class MainActivity extends AppCompatActivity {
 
     public void onCLick(View view) {
         sendNotification();
+
     }
 
   public void  createNotification(){
